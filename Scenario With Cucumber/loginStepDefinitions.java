00b49cec52d1ef6d1f1ed650e3803eab2b377249
fernandoiquiza <fernando.iquiza@fundacion-jala.org>
2017-01-09 09:50:06 -0400

    Added the Step Definitions for Scenario login
    This file contains the step definitions for each step in the
    loginScenario
    
    This should be in the same package as the loginRunner class.

---
+++ Scenario With Cucumber/loginStepDefinitions.java
@@ -0,0 +1,56 @@
+package org.fundacion.cucumber.login.login;
+
+
+import org.fundacion.common.drivers.Driver;
+import org.fundacion.pages.login.LoginPage;
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by Fernando on 1/9/2017.
+ */
+public class loginStepDefinitions {
+  WebDriver driver;
+  LoginPage login;
+
+  @Given("^I navigate to Pivotal Tracker Website$")
+  public void OpenPivotalTracker() throws IOException {
+    driver = Driver.getDriver().openBrowser();
+    login = new LoginPage(driver);
+  }
+
+  @When("^I set a valid username/email as (.*?)$")
+  public void ValidUsernameOrEmailIsSet(String userName) {
+    login.setUserName(userName);
+  }
+
+  @And("^I press NEXT button a new password field should appear$")
+  public void ANewPasswordFieldAppears() {
+    login.clickContinue();
+  }
+
+  @And("^I set a valid password as (.*?)$")
+  public void ValidPasswordIsSet(String password) {
+    login.setPassword(password);
+  }
+
+  @And("^I press the SIGN IN button$")
+  public void SignInButtonIsPressed(){
+    login.clickSubmit();
+  }
+
+  @Then("^I should be redirected to the Pivotal Dashboard and see the url as (.*?)$")
+  public void I_should_see_that_I_Logged_on_this_url(String url) {
+    String actualResult = driver.getCurrentUrl();
+    assertEquals(actualResult, url);
+  }
+}
